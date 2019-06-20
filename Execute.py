e9746435239fb38443aab07ad0275e54ed4d4eeb
Peng Yuan <826079925@qq.com>
2019-06-20 11:51:01 +0800

    修改处理预设参数的方法

--- Execute.py
+++ Execute.py
@@ -10,7 +10,6 @@ import time
 from parse_dict import *
 import logger
 import sys
-import deal_var
 from HTML import *
 reload(sys)
 sys.setdefaultencoding("utf-8")
@@ -68,6 +67,7 @@ def get_case(sheet_list, table):
 
 
 def api_run(table, case_num, logr, logl):
+    print "run case " + str(case_num)
     global pre_case_list, pre_recv, pre_var
     global titledict
     if not titledict:
@@ -75,17 +75,17 @@ def api_run(table, case_num, logr, logl):
     caseinfo = table.row_values(case_num)
     modelinfo = (caseinfo, titledict, table, pre_recv, logr, logl)
     url_addr = caseinfo[titledict["URL_ADDR"]]
-    url_addr, pre_recv = deal_var.deal_var_nodict(url_addr, modelinfo)
+    url_addr, pre_recv = deal_var_nodict(url_addr, modelinfo)
     url = caseinfo[titledict["域名IP及端口"]] + url_addr
     msg = caseinfo[titledict["REQUEST_MESSAGE"]]
     try:
         msg_loads = json.loads(msg)
     except:
         msg_loads = None
-    if msg_loads is not None:
-        msg_loads, pre_recv = deal_var.deal_var_dict(msg, msg_loads, modelinfo)
+    if type(msg_loads) is dict:
+        msg_loads, pre_recv = deal_var_dict(msg, msg_loads, modelinfo)
     else:
-        msg, pre_recv = deal_var.deal_var_nodict(msg, modelinfo)
+        msg, pre_recv = deal_var_nodict(str(msg), modelinfo)
         msg_loads = msg
     http_test = HTTP_API.HTTP_Cls(table.name)
     if caseinfo[titledict["请求方法"]].upper() == "GET":
@@ -118,6 +118,75 @@ def api_run(table, case_num, logr, logl):
     print pre_case_list
     pre_recv = recv_msg
     return recv_msg
+
+
+def deal_var_dict(msg, msg_loads, modelinfo):
+    var_list = re.findall(r'".*?":\s+?"\${.*?}"', msg)
+    key_list = []
+    value_list = []
+    caseinfo, titledict, table, pre_recv, logr, logl = modelinfo
+    if var_list:
+        for item in var_list:
+            value_list.append(item.split("${")[-1].strip('}"'))
+            key_list.append(item.split("${")[0].strip('"').strip().strip(':').strip('"'))
+        if caseinfo[titledict["前置条件"]]:  # 表格内多个前置条件用空格隔开
+            for pre_case in str(caseinfo[titledict["前置条件"]]).split():
+                pre_case = int(float(pre_case))
+                if pre_case in pre_case_list:
+                    pass
+                else:
+                    pre_case_list.append(pre_case)
+                    pre_recv = api_run(table, pre_case, logr, logl)
+                for pre_condition in value_list:
+                    if pre_condition not in pre_var.keys():
+                        # pre_var[pre_condition] = Check(pre_condition, msg_loads)
+                        pre_var[pre_condition] = eval("pre_recv" + search_dict(pre_condition, pre_recv))
+        for var in value_list:
+            var_key = key_list[value_list.index(var)]
+            if var == "timestamp":
+                exec ("msg_loads" + search_dict(var_key, msg_loads) + "=" + time.time())
+            else:
+                exec ("msg_loads" + search_dict(var_key, msg_loads) + "='" + str(pre_var[var]) + "'")
+    else:
+        if caseinfo[titledict["前置条件"]]:
+            for pre_case in str(caseinfo[titledict["前置条件"]]).split():
+                pre_case = int(float(pre_case))
+                if pre_case in pre_case_list:
+                    pass
+                else:
+                    pre_case_list.append(pre_case)
+                    pre_recv = api_run(table, pre_case, logr, logl)
+    return msg_loads, pre_recv
+
+
+def deal_var_nodict(msg, modelinfo):
+    caseinfo, titledict, table, pre_recv, logr, logl = modelinfo
+    var_list = re.findall(r'"\${(.*?)}"', msg)
+    if var_list:
+        if caseinfo[titledict["前置条件"]]:
+            for pre_case in str(caseinfo[titledict["前置条件"]]).split():
+                pre_case = int(float(pre_case))
+                if pre_case in pre_case_list:
+                    pass
+                else:
+                    pre_case_list.append(pre_case)
+                    pre_recv = api_run(table, pre_case, logr, logl)
+                for pre_condition in var_list:
+                    if pre_condition not in pre_var.keys():
+                        # pre_var[pre_condition] = Check(pre_condition, msg_loads)
+                        pre_var[pre_condition] = eval("pre_recv" + search_dict(pre_condition, pre_recv))
+        for var in var_list:
+            msg = msg.replace('${' + str(var) + '}', pre_var[var])
+    else:
+        if caseinfo[titledict["前置条件"]]:
+            for pre_case in str(caseinfo[titledict["前置条件"]]).split():
+                pre_case = int(float(pre_case))
+                if pre_case in pre_case_list:
+                    pass
+                else:
+                    pre_case_list.append(pre_case)
+                    pre_recv = api_run(table, pre_case, logr, logl)
+    return msg, pre_recv
 
 
 def check_result(recv_msg, caseinfo):
